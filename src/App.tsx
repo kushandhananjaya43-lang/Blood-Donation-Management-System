da3d1c578168ccaf53fe60582bd5bf7f510ed3a4
Sudana Dahamneth <sudanadahamneth07@gmail.com>
2026-07-11 13:00:29 +0530

    feat: add route for donor dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/auth/Login';
 import SignUp from './components/auth/SignUp';
+import DonorDashboard from './components/donor/DonorDashboard'; // 1. Add this import
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/donor-dashboard" element={<DonorDashboard />} /> {/* 2. Add this route */}
       </Routes>
     </Router>
   );

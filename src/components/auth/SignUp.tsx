2393847a204b15ee96a12bb2f3a33ecc32af8353
Sudana Dahamneth <sudanadahamneth07@gmail.com>
2026-09-12 15:31:14 +0530

    jenkins fixed

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -19,7 +19,7 @@ const SignUp: React.FC = () => {
         setErrorMsg('');
 
         // Register user with Supabase Auth & attach metadata
-        const {_data, error } = await supabase.auth.signUp({
+        const {data : _data, error } = await supabase.auth.signUp({
             email,
             password,
             options: {
